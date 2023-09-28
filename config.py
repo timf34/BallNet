3bf4e592ec48a73f2a017c65b372eff0e7e6cb89
timf34 <farrelti@tcd.ie>
2023-09-28 23:49:10 +0100

    large refactor of config.py, we now use inheritance to make the code *much* clearer

--- config.py
+++ config.py
@@ -1,149 +1,128 @@
-from dataclasses import dataclass
-from typing import Tuple, List
+from dataclasses import dataclass, field
+from typing import Tuple
 import os
 import time
 
 BALL_LABEL = 1
-
 
 def get_datetime():
     return time.strftime("%d_%m_%Y__%H%M")
 
 
 @dataclass
-class Config:
+class BaseConfig:
     """
-    Configuration class for the program.
+    Base Config class
     """
-    aws: bool = False
-    aws_testing: bool = False
+    model_name = f'model_{get_datetime()}'
 
-    # Training parameters
-    only_ball_frames: bool = True  # TODO: Note: I think its baked in to just use ball frames.
-    ball_threshold: float = 0.7
+    # Model params
     device: str = 'cuda:0'
-    # TODO: I really need some sort of different script maybe to read from the config file and then set the attributes
-    #  for the config class for the different training runs I'm doing. I could have a fineuning script... an AWS script...
-    #  an AWS testing script... a local testing script... a local training script... a local fine tuning script... etc.
-    #  These could just be txt files, or whatever really.
-    lr: float = 5e-5  # 1e-4 is the default learning rate for Adam; change to 1e-5 when fine-tuning
+    device_type: bool = "cuda"
+    lr: float = 1e-4
 
-    run_validation: bool = True  # Whether to use the validation dataloaders or not
+    # Training parms
+    run_validation: bool = True
+    ball_threshold: float = 0.7
     use_amp: bool = True
-    device_type: bool = "cuda"  # "cuda" or "cpu" (for AMP, and note it can't be "cuda:0" or "cuda:1" etc.)
-
-    finetuning: bool = True
-
-    # Model constants
+    finetuning: bool = False
     neg_pos_ratio: int = 3
-    use_augmentation: bool = True  # This is vital for good performance I believe.
-    ball_delta: int = 3  # TODO: this isn't being used in footandball yet.
-    image_size: Tuple[int, int] = (720, 1280)
-    image_extension: str = '.jpg'  # TODO: note that this isn't being used yet either
-    model: str = 'fb1'
-
-    train_image_size = (720, 1280)
-    val_image_size = (1080, 1920    )
-    train_size = 0.9
-    val_size = 0.1
-
-    out_video: str = 'test.avi'
+    use_augmentation: bool = True
     save_pickle_training_stats: bool = False
 
+    # Data params
+    only_ball_frames: bool = True
+    train_image_size: Tuple[int, int] = (720, 1280)
+    val_image_size: Tuple[int, int] = (1080, 1920)
+    train_size: float = 0.9
+    val_size: float = 0.1
+    ball_bbox_size: int = 20
+
+    # Data constants
+    model_folder: str = 'models'
+    weights: str = r'C:\Users\timf3\PycharmProjects\BallNet2.0\models\model_20210221_2206_final.pth'
+    data_path: str = r'C:\Users\timf3\OneDrive - Trinity College Dublin\Documents\Documents\datasets\Datasets\Bohs\bohs-preprocessed'
+
     # Debugging params
-    save_epochs_every_n_epochs: int = 5
-    save_heatmaps: bool = False
-    number_of_heatmaps_to_save: int = 1
+    save_every_n_epochs: int = 5
     save_weights_when_testing: bool = False
 
 
-    # Augmentation constants
-    ball_label: int = 1
-    ball_bbox_size: int = 20
-
-    def __post_init__(self):
-        self.model_name = f'model_{get_datetime()}'
-
-        if self.aws:
-            # Paths
-            try:
-                # Note: this will be the 'bohs-preprocessed' folder in S3!
-                self.bohs_path = os.environ['SM_CHANNEL_TRAINING']
-            except KeyError as e:
-                raise KeyError('The environment variable SM_CHANNEL_TRAINING is not set - ensure you run the file ' 
-                               '`train_aws_estimator` and not just `train_detector.py`! Otherwise we\'re not using' 
-                               'AWS and our environment variables won\'t be set.') from e
-
-            self.model_folder = os.environ['SM_MODEL_DIR']
-            self.checkpoints_folder = '/opt/ml/checkpoints'  # This can be access during training on AWS (opt/ml/model can't be)
-
-            # TODO: ensure to add this to the 'bohs-preprocessed' folder in S3!
-            self.finetuning_weights_file_name: str = "model_06_03_2023__0757_35.pth"
-            self.finetuning_weights_path: str = os.path.join(self.bohs_path, self.finetuning_weights_file_name)  # "s3://bohs-preprocessed/model_22_11_2022__0202_90.pth"
-
-            # Training parameters
-            self.num_workers: int = 8
-            self.batch_size: int = 32
-            self.epochs: int = 20
-            self.whole_dataset: bool = True
-            self.dataset_size: int = -1  # This value should not be used, just a placeholder
-
-        elif self.aws_testing:
-            # This is a more lightweight (data-wise) version of the above, for testing on AWS.
-            try:
-                self.bohs_path = os.environ['SM_CHANNEL_TRAINING']
-            except KeyError as e:
-                raise KeyError('The environment variable SM_CHANNEL_TRAINING is not set - ensure you run the file '
-                               ' `train_aws_estimator` and not just `train_detector.py`! Otherwise we\'re not using'
-                               ' AWS and our environment variables won\'t be set.') from e
-
-            self.model_folder = os.environ['SM_MODEL_DIR']
-            self.checkpoints_folder = '/opt/ml/checkpoints'
-
-            # Training parameters
-            self.num_workers: int = 0
-            self.batch_size: int = 1
-            self.epochs: int = 10
-            self.whole_dataset: bool = False
-            self.dataset_size: int = 2
-
-        else:
-            # Paths
-            self.bohs_path = r'C:\Users\timf3\OneDrive - Trinity College Dublin\Documents\Documents\datasets\Datasets\Bohs\bohs-preprocessed'
-            self.weights: str = r'C:\Users\timf3\PycharmProjects\BallNet2.0\models\model_20210221_2206_final.pth'
-            self.model_folder = 'models'
-
-            self.finetuning_weights_path: str = r"C:\Users\timf3\PycharmProjects\BohsNet\models\model_22_11_2022__0202_90.pth"
-
-            # Training parameters
-            self.num_workers: int = 0
-            self.batch_size: int = 1
-            self.epochs: int = 10
-            self.save_weights: bool = True
-
-            self.whole_dataset: bool = False
-            if self.whole_dataset:
-                print("WARNING: You are training on the whole dataset!!! Don't expect fast batches!")
-
-            self.dataset_size = 2  # Size of dataset if whole_dataset is False, not it gets n from each folder!
 
     def print(self):
         """
-        Prints the config class and all its attributes to the console, including those that are initialized in
-        the __post_init__ method.
+        Prints the config class and all its attributes to the console, including those from the __post_init__ method.
         """
         for i in self.__dict__:
             print(f'{i}: {self.__dict__[i]}')
 
-    def __str__(self):
-        """
-        Returns a string representation of the config class and all its attributes.
-        """
-        return str(self.__dict__)
+
+@dataclass
+class LaptopConfig(BaseConfig):
+    """Config for local development on laptop"""
+    # Training params
+    num_workers: int = 0
+    batch_size: int = 1
+    epochs: int = 10
+    save_weights: bool = True
+
+    # Data params
+    whole_dataset: bool = False
+    dataset_size: int = 2
+
+
+@dataclass
+class AWSBaseConfig(BaseConfig):
+    """Base Config for training on AWS SageMaker"""
+    aws_testing: bool = False  # For test runs, not full training runs
+
+    # Paths
+    checkpoints_folder = '/opt/ml/checkpoints'  # This can be access during training on AWS (opt/ml/model can't be)
+
+    def __post_init__(self):
+
+        try:
+            self.model_folder = os.environ['SM_MODEL_DIR']
+        except KeyError as e:
+            raise KeyError('The environment variable SM_MODEL_DIR is not set - ensure you run the file '
+                       '`train_aws_estimator` and not just `train_detector.py`! Otherwise we\'re not using'
+                       'AWS and our environment variables won\'t be set.') from e
+        try:
+            self.bohs_path = os.environ['SM_CHANNEL_TRAINING']  # The `bohs-preprocessed` folder in S3
+        except KeyError as e:
+            raise KeyError('The environment variable SM_CHANNEL_TRAINING is not set - ensure you run the file '
+                       '`train_aws_estimator` and not just `train_detector.py`! Otherwise we\'re not using'
+                       'AWS and our environment variables won\'t be set.') from e
+
+
+@dataclass
+class AWSTestConfig(AWSBaseConfig):
+    """Config for testing on AWS"""
+    num_workers: int = 0
+    batch_size: int = 1
+    epochs: int = 10
+    whole_dataset: bool = False
+    dataset_size: int = 2
+
+
+@dataclass
+class AWSTrainConfig(AWSBaseConfig):
+    """Full training run on AWS"""
+    num_workers: int = 8
+    batch_size: int = 32
+    epochs: int = 20
+    whole_dataset: bool = True
+    dataset_size: int = -1  # Just a placeholder
+
+
+
+@dataclass
+class AWSFineTuningConfig(AWSBaseConfig):
+
+    finetuning_weights_filename: str = "model_06_03_2023__0757_35.pth"
+    # finetuneing_weights_path: str = os.path.join(bohs_path, finetuning_weights_file_name)  # "s3://bohs-preprocessed/model_22_11_2022__0202_90.pth"
 
 
 if __name__ == '__main__':
-    config = Config()
-    print(config.model_name)
-    print(config)
-    config.print()
+    x = LaptopConfig()
+    print(x)
