b8e422d2c3cc2c771103827c1ec66cd7b65dd5a7
timf34 <farrelti@tcd.ie>
2023-10-14 04:00:45 +0100

    removed unsued collate_fn func

--- data/cvat_dataloaders.py
+++ data/cvat_dataloaders.py
@@ -28,7 +28,7 @@ def make_data_loader(config: BaseConfig, modes: List[str], use_hardcoded_data_fo
             batch_size=config.batch_size,
             shuffle=True,
             num_workers=config.num_workers,
-            collate_fn=collate_fn,
+            collate_fn=my_collate,
         )
     return data_loader
 
@@ -40,23 +40,6 @@ def my_collate(batch):
     # image_path = [e[3] for e in batch]
     # return images, boxes, labels, image_path
     return images, boxes, labels
-
-
-def collate_fn(batch):
-    """Custom collate fn for dealing with batches of images that have a different
-    number of associated object annotations (bounding boxes).
-
-    Arguments:
-        batch: (tuple) A tuple of tensor images and lists of annotations
-
-    Return:
-        A tuple containing:
-            1) (tensor) batch of images stacked on their 0 dim
-            2) (list of tensors) annotations for a given image are stacked on
-                                 0 dim
-    """
-    images, targets = zip(*batch)
-    return torch.stack(images, 0), targets
 
 
 # Note: I'll come back to this for refactoring later as we won't be using it right now, and its quite a mess.
