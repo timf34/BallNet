d1fb86332ca59274cd34c5a7eafe8526225e6601
timf34 <farrelti@tcd.ie>
2023-10-16 15:50:06 +0100

    working eval.py; but needs big cleanup

--- eval.py
+++ eval.py
@@ -1,81 +1,115 @@
 """
 What do we need:
 
-- BohsDataset: object which allows me to specify which dataset I want to use (so I can use a specific camera) (might need slightl adjustment)
-    - Ok so we basically have this right now, I can add better features later.
-- BohsNet: the CNN (done)
-    - Let's get this bread.
-- BohsEval: class which encapsulates the evaluation process (including measuring metrics) (a good bit of this is already done)
+- CVATEval
+    - Encapsulates the Eval logic
+- CVATDataLoader
+- BallNet
 
+Functionality:
+
+- Load the dataloader and specify eval/ test mode (just do "test" for now)
 """
 import torch
 
+from dataclasses import dataclass
 from typing import List, Tuple, Dict
 
-from config import Config
-from data.cvat_dataloaders import make_dataloaders, make_eval_dataloader
+from config import BaseConfig, AFLLaptopConfig
+from data.cvat_dataloaders import make_data_loader
 from network.footandball import model_factory
 from data.cvat_utils import eval_single_frame
+from utils import set_seed
 
-class EvalConfig(Config):
-    def __init__(self):
-        super().__init__()
-        self.model_mode: str = 'detect'
+SEED: int = 42
+TOLERANCE: int = 5
 
-        # Overriding defaults we care about
-        self.aws: bool = False
-        self.aws_testing: bool = False
-        self.batch_size: int = 1
-        self.whole_dataset: bool = True
 
-def prepare_model(config: EvalConfig):
-    model = model_factory(config.model, config.model_mode, ball_threshold=config.ball_threshold)
+@dataclass
+class EvalConfig(AFLLaptopConfig):
+    model_mode: str = "detect"
+    batch_size: int = 1
+    whole_dataset: bool = True
+    use_augmentations: bool = False
+    model_weights: str = "models/model_15_10_2023__1154/model_15_10_2023__1154_100.pth"
+
+
+def prepare_eval_model(config: EvalConfig) -> torch.nn.Module:
+    model = model_factory('fb1', config.model_mode, ball_threshold=config.ball_threshold)
+    model.load_state_dict(torch.load(config.model_weights))
     model.eval()
     model.to(config.device)
     return model
 
-def get_ball_list_from_dets(detections) -> List[Tuple[int, int]]:
-    """
-    This function takes in a list of detections, of the form Tensor.size(n,4) where n is the number of detections,
-    and returns a list of tuples of the form (x, y) for each detection.
-    :param detections: Tensor.size(n,4)
-    :return: List[Tuple[int, int]]
-    """
-    dets = []
-    for box in detections:
-        if len(box) == 4:
-            x1, y1, x2, y2 = box
-            x = (x1 + x2) / 2
-            y = (y1 + y2) / 2
-            x, y = int(x), int(y)
-            dets.append((x, y))
-        else:
-            print(box)
-            # Check if the box is tensor of size=(0, 4))]
-    return dets
 
+
+# def box_to_xy(box: List[int]) -> Tuple[int, int]:
+#     x1, y1, x2, y2 = box
+#     return int((x1 + x2) / 2), int((y1 + y2) / 2)
+
+def box_to_xy(box: List[int]) -> Tuple[int, int]:
+    if len(box) != 4:
+        box = box[0]
+    x1, y1, x2, y2 = box
+    x = int((x1 + x2) / 2)
+    y = int((y1 + y2) / 2)
+    return x, y
+
+# TODO: this is a mess rn, will clean up later. Just get something basic working. Go for a walk, then come back and fix
+#  things up properly. If this is the only thing I get done today, that's not too bad.
+
+# TODO: note that there is still some empty labels in the val dataloader!! Need to look into this!
+
+# Note: will clean things up afterwards here
 def main():
     config = EvalConfig()
-    eval_dataloader = make_eval_dataloader(config)
-    model = prepare_model(config)
+    data_loader = make_data_loader(config, modes=["val"], use_hardcoded_data_folders=False)
+    model = prepare_eval_model(config)
+
+    dataloader_keys = list(data_loader.keys())
 
     frame_stats = []
 
-    for count, (images, boxes, labels) in enumerate(eval_dataloader):
-        imgaes = images.to(config.device)
-        boxes = get_ball_list_from_dets()
+    for keys in dataloader_keys:
+        for count, (image, boxes, labels) in enumerate(data_loader[keys]):
+            image = image.to(config.device)
+            detections = model(image)
+            print(boxes)
+            print(boxes[0].tolist())
+            print(detections)
 
-        with torch.no_grad():
-            dets = model(images)[0]
+            if boxes[0].numel() == 0:
+                # Pass if there are no detections
+                continue
 
-            if len(dets["boxes"]) != 0:
-                ball_dets: List[Tuple[int, int]] = get_ball_list_from_dets(dets["boxes"])
-            else:
-                # No detections
-                ball_dets = []
-            frame_stats.append(eval_single_frame(ball_dets, boxes, tolerance=3))
+            gt_ball_pos = box_to_xy(boxes[0].tolist())
 
+            if len(detections[0]['boxes']) == 0:
+                pred_ball_pos = None
+
+            # Note: this only works for the 1st prediction right now.
+            for box, label, score in zip(detections[0]['boxes'], detections[0]['labels'], detections[0]['scores']):
+                pred_ball_pos = [box_to_xy(box)]
+
+            frame_stats.append(eval_single_frame(ball_pos=pred_ball_pos, gt_ball_pos=[gt_ball_pos], tolerance=5))
+
+
+    print(frame_stats)
+
+    percent_correctly_classified_frames = sum(
+        c for (_, _, c) in frame_stats
+    ) / len(frame_stats)
+    temp = [p for (p, _, _) in frame_stats if p is not None]
+    avg_precision = sum(temp) / len(temp)
+
+    temp = [r for (_, r, _) in frame_stats if r is not None]
+    avg_recall = sum(temp) / len(temp)
+
+    print(f"Percent correctly classified frames: {percent_correctly_classified_frames}")
+    print(f"Average precision: {avg_precision}")
+    print(f"Average recall: {avg_recall}")
 
 
 if __name__ == '__main__':
+    set_seed(SEED)
     main()
